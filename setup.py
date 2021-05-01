381f5c09c4e4b1b6664234dc5c53c71f2d071787
Magnus Nordlander <magnus@nordlander.se>
2021-05-01 07:54:03 +0200

    Updated packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name = 'pygluehome',
-    install_requires=['asyncio', 'aiohttp>=3.7.4', 'iso8601>=0.1.14'],
-    version = '0.1.0',
-    description = 'A library to communicate with Glue Lock',
-    author='Magnus Nordlander',
-    url='https://github.com/magnusnordlander/pygluehome/',
-)
+setuptools.setup()
